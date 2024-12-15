db79b00be358632075f29402c5731c64f7346928
Ziwei Zhu <ziwei.zhu777@gmail.com>
2024-12-15 17:33:47 +0800

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Ziwei Zhu
+ * @Date: 2024-12-15 16:42:42
+ * @Last Modified by: Ziwei Zhu
+ * @Last Modified time: Do not edit
+ */
+module.exports = {
+  content: ['./**/*.hbs'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
